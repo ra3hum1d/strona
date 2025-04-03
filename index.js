ea17e25d176a3bc66d87a58a259163f7053bdb04
Roman <hilixhilix1@gmail.com>
2025-04-03 13:02:02 +0200

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 var swiper = new Swiper(".mySwiper", {
   loop: true,
   centeredSlides: true,
-  slidesPerView: "auto", 
+  slidesPerView: "3",
   spaceBetween: 20,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
   },
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+  autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
   },
   on: {
-    slideChangeTransitionEnd: function () {
-      document.querySelectorAll('.swiper-slide').forEach((slide, index) => {
-        if (slide.classList.contains('swiper-slide-prev') || slide.classList.contains('swiper-slide-next')) {
-          slide.style.transform = 'scale(0.6)';
-          slide.style.opacity = '0.3';
-        } else {
-          slide.style.transform = 'scale(1)';
-          slide.style.opacity = '1';
-        }
-      });
-    }
+      init: function () {
+          this.slides.forEach((slide, index) => {
+              if (!slide.classList.contains('swiper-slide-active')) {
+                  slide.style.transform = 'scale(0.6)';
+                  slide.style.opacity = '0.3';
+              }
+          });
+      },
+      slideChangeTransitionEnd: function () {
+          this.slides.forEach((slide, index) => {
+              if (slide.classList.contains('swiper-slide-prev') || slide.classList.contains('swiper-slide-next')) {
+                  slide.style.transform = 'scale(0.6)';
+                  slide.style.opacity = '0.3';
+              } else {
+                  slide.style.transform = 'scale(1)';
+                  slide.style.opacity = '1';
+              }
+          });
+      }
   }
 });
