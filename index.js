e5155c43b4ccb8a23fc687c4166088806ccf3bdc
Roman <hilixhilix1@gmail.com>
2025-04-10 22:43:12 +0200

    first commit

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
 var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  centeredSlides: true,
-  slidesPerView: "3",
-  spaceBetween: 20,
-  navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-  },
-  autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-  },
-  on: {
-      init: function () {
-          this.slides.forEach((slide, index) => {
-              if (!slide.classList.contains('swiper-slide-active')) {
-                  slide.style.transform = 'scale(0.6)';
-                  slide.style.opacity = '0.3';
-              }
-          });
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: "3",
+    spaceBetween: 20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        centeredSlides: false,
       },
-      slideChangeTransitionEnd: function () {
-          this.slides.forEach((slide, index) => {
-              if (slide.classList.contains('swiper-slide-prev') || slide.classList.contains('swiper-slide-next')) {
-                  slide.style.transform = 'scale(0.6)';
-                  slide.style.opacity = '0.3';
-              } else {
-                  slide.style.transform = 'scale(1)';
-                  slide.style.opacity = '1';
-              }
-          });
+      481: {
+        slidesPerView: 3,
+        centeredSlides: true,
       }
-  }
-});
+    },
+    on: {
+        init: function () {
+            this.slides.forEach((slide, index) => {
+                if (!slide.classList.contains('swiper-slide-active')) {
+                    slide.style.transform = 'scale(0.6)';
+                    slide.style.opacity = '0.3';
+                }
+            });
+        },
+        slideChangeTransitionEnd: function () {
+            this.slides.forEach((slide, index) => {
+                if (slide.classList.contains('swiper-slide-prev') || slide.classList.contains('swiper-slide-next')) {
+                    slide.style.transform = 'scale(0.6)';
+                    slide.style.opacity = '0.3';
+                } else {
+                    slide.style.transform = 'scale(1)';
+                    slide.style.opacity = '1';
+                }
+            });
+        }
+    }
+  });
+
+
+
+//   ========
 
 
 
